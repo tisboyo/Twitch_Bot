1745c9905dbde9a6343d28136e71a72e4d138cd6
Tisboyo <10230718+tisboyo@users.noreply.github.com>
2020-09-07 20:50:44 -0400

    Fix helix link

--- keygen_links.py
+++ keygen_links.py
@@ -6,7 +6,7 @@ with open("configs/config.json") as f:
 client_id = data["client_id"]
 
 irc = f"https://id.twitch.tv/oauth2/authorize?response_type=token&client_id={client_id}&redirect_uri=https://twitchapps.com/tokengen/&scope=chat:read+chat:edit+channel:moderate+whispers:read+whispers:edit+channel_editor"
-helix = f"https://id.twitch.tv/oauth2/authorize?response_type=token&client_id={client_id}&redirect_uri=https://twitchapps.com/tokengen/&scope=channel%3Aread%3Aredemptions%20channel%3Amoderate%20bits%3Aread%20channel_subscriptions"
+helix = f"https://id.twitch.tv/oauth2/authorize?response_type=token&client_id={client_id}&redirect_uri=https://twitchapps.com/tokengen/&scope=channel:read:redemptions+channel:moderate+bits:read+channel_subscriptions"
 
 print("Use this link for IRC OAuth")
 print(irc)
