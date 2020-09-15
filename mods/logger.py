0bc2c1c9976719f7d277556841a0c955f00da574
Tisboyo <10230718+tisboyo@users.noreply.github.com>
2020-09-14 23:51:19 -0400

    Update logger to include last_message and message_count tracking

--- mods/logger.py
+++ mods/logger.py
@@ -1,5 +1,9 @@
 import asyncio
 from aiofile import AIOFile
+import json
+from datetime import datetime, date
+
+from data import load_data, save_data
 
 from twitchbot import Event, Message, Mod, cfg
 
@@ -12,7 +16,33 @@ class TwitchLog(Mod):
     async def on_raw_message(self, msg: Message):
 
         if True:
-            async with AIOFile("irc.log", "a") as afp:
-                await afp.write(str(msg) + "\n")
+            async with AIOFile(
+                f"irc_logs/{date.today().isoformat()}-irc.log", "a"
+            ) as afp:
+                await afp.write(f"{datetime.now().isoformat()}:{msg} \n")
                 await afp.fsync()
+
+        # If the message is a system message, we're done here
+        if not msg.author:
+            return
+
+        # Create an empty object if the user is new
+        if not msg.author in self.user_data:
+            self.user_data[msg.author] = {"last_message": None, "message_count": 0}
+
+        author = self.user_data[msg.author]
+
+        # Track users last message time
+        author["last_message"] = datetime.now()
+
+        # Track the number of messages total from the user
+        author["message_count"] += 1
+
+        await save_data("user", self.user_data)
+
+    async def on_connected(self):
+        """Load user data from file when connected"""
+        print("Loading user data from json file...", end="")
+        self.user_data = await load_data("user")
+        print("done")
 
