87374e09c4f062ce625587d7e85ab493a3cc21d8
Tisboyo <10230718+tisboyo@users.noreply.github.com>
2020-09-15 00:05:08 -0400

    clarifications for auth links

--- keygen_links.py
+++ keygen_links.py
@@ -6,10 +6,10 @@ with open("configs/config.json") as f:
 client_id = data["client_id"]
 
 irc = f"https://id.twitch.tv/oauth2/authorize?response_type=token&client_id={client_id}&redirect_uri=https://twitchapps.com/tokengen/&scope=chat:read+chat:edit+channel:moderate+whispers:read+whispers:edit+channel_editor"
-helix = f"https://id.twitch.tv/oauth2/authorize?response_type=token&client_id={client_id}&redirect_uri=https://twitchapps.com/tokengen/&scope=channel:read:redemptions+channel:moderate+bits:read+channel_subscriptions"
+pubsub = f"https://id.twitch.tv/oauth2/authorize?response_type=token&client_id={client_id}&redirect_uri=https://twitchapps.com/tokengen/&scope=channel:read:redemptions+channel:moderate+bits:read+channel_subscriptions"
 
-print("Use this link for IRC OAuth")
+print("Use this link for IRC OAuth - Use the BOT Account")
 print(irc)
 print("-" * 100)
-print("Use this link for PubSub Oauth")
-print(helix)
+print("Use this link for PubSub Oauth - Use the Streamer Account")
+print(pubsub)
