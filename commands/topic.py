8219c40a0e8bc1ba1561669f19199f7aa6cfb8ff
Tisboyo <10230718+tisboyo@users.noreply.github.com>
2020-09-19 15:47:10 -0400

    Add ability to trigger topic from whisper

--- commands/topic.py
+++ commands/topic.py
@@ -1,18 +1,26 @@
-from twitchbot import Command, CommandContext
+from twitchbot import Command, CommandContext, channels, cfg
 
 from data import save_data, load_data
 
 save_file = "topic"
 
 
-@Command("topic")
+@Command("topic", context=CommandContext.BOTH)
 async def topic(msg, *args):
     topic = await load_data(save_file)
-    try:
-        await msg.reply(topic["topic"])
-    except KeyError:
-        # No topic was set
-        await msg.reply("No current topic.")
+
+    if not msg.is_whisper:
+        try:
+            await msg.reply(topic["topic"])
+        except KeyError:
+            # No topic was set
+            await msg.reply("No current topic.")
+    else:
+        try:
+            # Send message directly to channel 0
+            await channels[cfg.channels[0]].send_message(topic["topic"])
+        except Exception as e:
+            print(e)
 
 
 @Command("set_topic", permission="admin", context=CommandContext.BOTH)
