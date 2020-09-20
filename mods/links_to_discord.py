a28740f52123b26555d75d0d39dfd98dca7d3df5
Tisboyo <10230718+tisboyo@users.noreply.github.com>
2020-09-20 14:03:37 -0400

    Include user name in bot name when posting link

--- mods/links_to_discord.py
+++ mods/links_to_discord.py
@@ -1,17 +1,4 @@
-from twitchbot import (
-    cfg,
-    Mod,
-    task_exist,
-    stop_task,
-    add_task,
-    channels,
-    ModCommand,
-    Message,
-)
-from asyncio import sleep
-from aiofile import AIOFile
-import json
-from datetime import datetime
+from twitchbot import cfg, Mod, channels, Message
 import re
 import aiohttp
 
@@ -25,13 +12,13 @@ class LinksToDiscord(Mod):
     def find_url(self, string):
         # findall() has been used
         # with valid conditions for urls in string
-        regex = r"(?i)\b((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'\".,<>?«»“”‘’]))"
+        regex = r"(?i)\b((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'\".,<>?«»“”‘’]))"  # noqa:E501
         url = re.findall(regex, string)
         return [x[0] for x in url]
 
     async def on_privmsg_received(self, msg: Message):
         # Make sure the webhook url is set
-        if cfg.webhookurl == None:
+        if cfg.webhookurl is None:
             return
 
         webhook = cfg.webhookurl
@@ -41,7 +28,7 @@ class LinksToDiscord(Mod):
         if len(urls):
             for each in urls:
                 async with aiohttp.ClientSession() as session:
-                    response = await session.post(webhook, json={"content": each, "username": "TwitchBot"})
+                    response = await session.post(webhook, json={"content": each, "username": f"TwitchBot for {msg.author}"})
                     if response.status == 204:
                         message = "Thanks for the link, I posted it to discord."
                         await channels[cfg.channels[0]].send_message("🤖" + message)
