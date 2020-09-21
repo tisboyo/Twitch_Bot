abaf02641a84fde51d60ce28bd64a770b56f6408
Tisboyo <10230718+tisboyo@users.noreply.github.com>
2020-09-20 21:08:36 -0400

    Ignore bots that post links

--- mods/links_to_discord.py
+++ mods/links_to_discord.py
@@ -7,7 +7,9 @@ class LinksToDiscord(Mod):
     name = "linkstodiscord"
 
     def __init__(self):
-        pass
+        super().__init__()
+        print("LinksToDiscord loaded")
+        self.webhook = cfg.webhookurl
 
     def find_url(self, string):
         # findall() has been used
@@ -17,18 +19,18 @@ class LinksToDiscord(Mod):
         return [x[0] for x in url]
 
     async def on_privmsg_received(self, msg: Message):
-        # Make sure the webhook url is set
-        if cfg.webhookurl is None:
+        # Webhook not configured  or Ignore these users
+        if self.webhook is None or msg.author in ["addohms", "streamlabs"]:
             return
-
-        webhook = cfg.webhookurl
 
         urls = self.find_url(msg.content)
 
         if len(urls):
             for each in urls:
                 async with aiohttp.ClientSession() as session:
-                    response = await session.post(webhook, json={"content": each, "username": f"TwitchBot for {msg.author}"})
+                    response = await session.post(
+                        self.webhook, json={"content": each, "username": f"TwitchBot for {msg.author}"}
+                    )
                     if response.status == 204:
                         message = "Thanks for the link, I posted it to discord."
                         await channels[cfg.channels[0]].send_message("🤖" + message)
