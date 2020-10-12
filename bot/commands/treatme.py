3926866dcf086e9097a5d2f8d6d12dbddb40b988
Tisboyo <10230718+tisboyo@users.noreply.github.com>
2020-10-12 06:47:00 +0000

    Add `!treatme cooldown` command Saves the value to the database Settings table, with a key of `treatbot_mqtt_cooldown` Adds cooldown for issue #20

--- bot/commands/treatme.py
+++ bot/commands/treatme.py
@@ -1,12 +1,49 @@
 from main import AddOhmsBot
 from twitchbot import Command
+from twitchbot import SubCommand
+from twitchbot.database.session import session
+
+from mods.database_models import Settings
+
+# Key that is sent to Adafruit.IO
+aio_key = "dispense-treat-toggle"
+
+# Set a default cooldown
+q = session.query(Settings.value).filter(Settings.key == "treatbot_mqtt_cooldown").one_or_none()
+if q is None:
+    # Value wasn't in the database, lets insert it.
+    insert = Settings(key="treatbot_mqtt_cooldown", value=300)
+    session.add(insert)
+    AddOhmsBot.AIO.mqtt_cooldown[aio_key] = 300
+else:
+    AddOhmsBot.AIO.mqtt_cooldown[aio_key] = int(q[0])
 
 
 @Command("treatme")
 async def push_treat(msg, *args):
-    if AddOhmsBot().AIO.send("dispense-treat-toggle"):
+    if AddOhmsBot().AIO.send(aio_key):
         await msg.reply(f"{AddOhmsBot.msg_prefix}Teleporting a treat")
     else:
         await msg.reply(f"{AddOhmsBot.msg_prefix}I couldn't do that at the moment. Sorry ☹️")
 
-    # baldaio.push_attn('dispense-treat-toggle')
+
+@SubCommand(push_treat, "cooldown", permission="admin")
+async def treatme_cooldown(msg, *args):
+    cooldown = AddOhmsBot.AIO.mqtt_cooldown
+
+    try:
+        new_cooldown = int(args[0])
+        cooldown[aio_key] = new_cooldown
+        session.query(Settings).filter(Settings.key == "treatbot_mqtt_cooldown").update({"value": new_cooldown})
+        await msg.reply(f"Treatme cooldown changed to {new_cooldown}")
+
+    except IndexError:
+        await msg.reply(f"Current cooldown is {cooldown[aio_key]} seconds.")
+        return
+
+    except ValueError:
+        await msg.reply("Invalid value.")
+
+    except Exception as e:
+        print(type(e), e)
+        return
