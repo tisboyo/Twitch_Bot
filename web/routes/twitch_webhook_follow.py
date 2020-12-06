07a3f485badec3e74e81a66ff3c342f8123fa967
tisboyo <10230718+tisboyo@users.noreply.github.com>
2020-12-06 02:40:08 +0000

    Add bot connection error handling

--- web/routes/twitch_webhook_follow.py
+++ web/routes/twitch_webhook_follow.py
@@ -3,6 +3,7 @@ from os import getenv
 
 from fastapi import APIRouter
 from fastapi import Request
+from fastapi.exceptions import HTTPException
 from fastapi.responses import PlainTextResponse
 
 router = APIRouter()
@@ -22,15 +23,23 @@ async def twitch_webhook_follow_post(body: dict):
     data = body["data"][0]
 
     if data.get("from_name", False):
-        s = socket.socket()
-        s.connect(("bot", 13337))
+        try:
+            s = socket.socket()
+            s.connect(("bot", 13337))
 
-        # We don't actually need this, but we have to read it before the bot will accept commands
-        s.recv(300).decode("utf8")
+            # We don't actually need this, but we have to read it before the bot will accept commands
+            s.recv(300).decode("utf8")
 
-        # Send the channel that we are connecting to
-        s.send(f"{getenv('TWITCH_CHANNEL')}\n".encode("utf8"))
-        s.send(f"🤖 Thanks for the follow @{data['from_name']}\n".encode("utf8"))
+            # Send the channel that we are connecting to
+            s.send(f"{getenv('TWITCH_CHANNEL')}\n".encode("utf8"))
+            s.send(f"🤖 Thanks for the follow @{data['from_name']}\n".encode("utf8"))
 
-        # Close the socket
-        s.shutdown(socket.SHUT_RD)
+            # Close the socket
+            s.shutdown(socket.SHUT_RD)
+        except ConnectionRefusedError:
+            print("Unable to connect to bot for new follow.")
+            raise HTTPException(status_code=503)
+
+        except Exception as e:
+            print(f"Unknown exception trying to send new follow to bot. {e}")
+            raise HTTPException(status_code=503)
