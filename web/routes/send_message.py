10cf3346b3bf70e28e44a9262f8cfd27407f6e74
tisboyo <10230718+tisboyo@users.noreply.github.com>
2020-12-06 01:08:01 +0000

    External call to make the bot say stuff Fixes #59

--- web/routes/send_message.py
+++ web/routes/send_message.py
@@ -2,14 +2,34 @@ import socket
 from os import getenv
 
 from fastapi import APIRouter
+from fastapi.exceptions import HTTPException
+from fastapi.params import Body
+from fastapi.params import Depends
+from fastapi.params import Security
 from fastapi.responses import JSONResponse
+from fastapi.security.api_key import APIKeyHeader
+from starlette.status import HTTP_401_UNAUTHORIZED
 
 router = APIRouter()
 
+API_KEY = getenv("WEB_API_KEY")
+API_KEY_NAME = "access_token"
+
+api_key_header = APIKeyHeader(name=API_KEY_NAME)
+
+
+async def check_for_valid_api_key(api_key_header: str = Security(api_key_header)):
+    if api_key_header == API_KEY:
+        return True
+    else:
+        raise HTTPException(status_code=HTTP_401_UNAUTHORIZED)
+
 
 @router.post("/post_message")
-def twitch_webhook_follow_post(body: dict):
-
+async def post_message(
+    body: dict = Body(..., example={"message": "Post your message here, it will be relayed to twitch chat"}),
+    authorized: bool = Depends(check_for_valid_api_key),
+):
     if body.get("message", False):
         try:
             s = socket.socket()
@@ -28,4 +48,8 @@ def twitch_webhook_follow_post(body: dict):
 
         except ConnectionRefusedError:
             print("Unable to connect to bot.")
-            return JSONResponse({"success": False, "response": "Unable to connect to bot."})
+            return JSONResponse({"success": False, "detail": "Unable to connect to bot."})
+
+        except Exception as e:
+            print(f"Unknown exception trying to send message to bot. {e}")
+            return JSONResponse({"success": False, "detail": e})
