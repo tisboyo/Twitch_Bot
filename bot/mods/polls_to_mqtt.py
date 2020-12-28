7b6bb864f4e9352042d66064f5648ee07816226c
tisboyo <10230718+tisboyo@users.noreply.github.com>
2020-12-28 06:13:09 +0000

    Add sending poll data to MQTT #65

---
+++ bot/mods/polls_to_mqtt.py
@@ -0,0 +1,59 @@
+import asyncio
+import json
+
+from main import AddOhmsBot
+from twitchbot.channel import Channel
+from twitchbot.modloader import Mod
+from twitchbot.poll.polldata import PollData
+
+
+class PollsToMQTT(Mod):
+    name = "PollsToMQTT"
+
+    def __init__(self):
+        super().__init__()
+        self.mqtt = AddOhmsBot.AIO
+
+    def get_poll_setup(self, poll: PollData, active: bool = True) -> str:
+        # Build the dictionary for poll setup
+        poll_setup = dict(
+            title=poll.title,
+            total_duration=poll.duration_seconds,
+            choices=poll.choices,
+            active=active,
+        )
+        return json.dumps(poll_setup)
+
+    def get_poll_data(self, poll: PollData) -> str:
+        # Build the dictionary for poll data
+        poll_data = dict(
+            seconds_left=poll.seconds_left,
+            votes=poll.votes,
+            done=poll.done,
+        )
+        return json.dumps(poll_data)
+
+    async def on_poll_started(self, channel: Channel, poll: PollData):
+
+        # Send the setup data to MQTT
+        setup_json = self.get_poll_setup(poll)
+        self.mqtt.send("stream/poll/setup", setup_json)
+
+        # While the poll is running, send continuous updates
+        while not poll.done:
+            data_json = self.get_poll_data(poll, active=False)
+            self.mqtt.send("stream/poll/data", data_json)
+            await asyncio.sleep(0.5)
+
+    async def on_poll_ended(self, channel: Channel, poll: PollData):
+
+        # Send final results
+        data_json = self.get_poll_data(poll)
+        self.mqtt.send("stream/poll/data", data_json)
+
+        # Sleep for 15 seconds before hiding the canvas
+        asyncio.sleep(15)
+
+        # Send the setup data to MQTT
+        setup_json = self.get_poll_setup(poll)
+        self.mqtt.send("stream/poll/setup", setup_json)
