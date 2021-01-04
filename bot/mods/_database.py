43a31b44bb1ab9f05328d4b744444e224988c8d4
tisboyo <10230718+tisboyo@users.noreply.github.com>
2021-01-04 02:17:30 +0000

    Update mysql to database when framework updates Fixes #71

--- bot/mods/_database.py
+++ bot/mods/_database.py
@@ -1,13 +1,15 @@
 from asyncio import sleep
 
-from models import Base
-from models import Settings
 from sqlalchemy import create_engine
 from sqlalchemy import orm
 from twitchbot import Mod
-from twitchbot.config import mysql_cfg
+from twitchbot.config import database_cfg
+from twitchbot.database.session import _try_get_env
 from twitchbot.util import add_task
 from twitchbot.util import task_running  # Put all Table classes above here
+
+from models import Base
+from models import Settings
 
 
 class Database(Mod):
@@ -16,9 +18,14 @@ class Database(Mod):
     def __init__(self) -> None:
         super().__init__()
 
-        engine = create_engine(
-            f"mysql+mysqlconnector://{mysql_cfg.username}:{mysql_cfg.password}@{mysql_cfg.address}:{mysql_cfg.port}/{mysql_cfg.database}"  # noqa E501
-        )
+        username = _try_get_env(database_cfg.username)
+        passwd = _try_get_env(database_cfg.password)
+        address = _try_get_env(database_cfg.address)
+        port = _try_get_env(database_cfg.port)
+        database = _try_get_env(database_cfg.database)
+        driver = _try_get_env(database_cfg.database_format) + "+" + _try_get_env(database_cfg.driver)
+
+        engine = create_engine(f"{driver}://{username}:{passwd}@{address}:{port}/{database}")
         Session = orm.sessionmaker(bind=engine, autoflush=True)
         self.session = orm.scoped_session(Session)
         Base.metadata.create_all(engine)
