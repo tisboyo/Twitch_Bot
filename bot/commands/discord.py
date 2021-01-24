6babd32814386ee056f6fc328fe2eb1db70c541d
tisboyo <10230718+tisboyo@users.noreply.github.com>
2021-01-24 01:50:09 +0000

    Remove discord hard coded command In favor of using command from database.

--- bot/commands/discord.py
+++
@@ -1,7 +0,0 @@
-from main import AddOhmsBot
-from twitchbot import Command
-
-
-@Command("discord")
-async def discord(msg, *args):
-    await msg.reply(f"{AddOhmsBot.msg_prefix}Keep the conversation going in discord at https://baldengineer.com/discord")
