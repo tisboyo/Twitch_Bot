5847b28af3e0b46dbb1ae5a01b4ed84fb08986a2
tisboyo <10230718+tisboyo@users.noreply.github.com>
2021-02-01 01:20:47 +0000

    Convert to Mod in preperation for #72

--- bot/mods/treats.py
+++ bot/mods/treats.py
@@ -1,36 +1,44 @@
+from helpers.points import Points
 from main import AddOhmsBot
-from twitchbot import Command
-from twitchbot import SubCommand
+from twitchbot.command import ModCommand
+from twitchbot.command import SubCommand
+from twitchbot.enums import CommandContext
+from twitchbot.modloader import Mod
 
 mqtt_topic = AddOhmsBot.MQTT.Topics.dispense_treat_toggle
 
 
-@Command("treatme")
-async def push_treat(msg, *args):
-    if await AddOhmsBot.MQTT.send(mqtt_topic):
-        await msg.reply(f"{AddOhmsBot.msg_prefix}Teleporting a treat")
-    else:
-        await msg.reply(f"{AddOhmsBot.msg_prefix}I couldn't do that at the moment. Sorry ☹️")
+class Treats(Mod):
+    name = "yay"
 
+    def __init__(self):
+        self.points = Points(emojis=5, unique_users=2, mod_commands=1, require_all=True, emoji_cap=3)
 
-@SubCommand(push_treat, "cooldown", permission="admin")
-async def treatme_cooldown(msg, *args):
-    try:
-        new_cooldown = int(args[0])
+    @ModCommand(name, "treatme", context=CommandContext.BOTH, permission="admin")
+    async def push_treat(self, msg, *args):
+        if await AddOhmsBot.MQTT.send(mqtt_topic):
+            await msg.reply(f"{AddOhmsBot.msg_prefix}Teleporting a treat")
+        else:
+            await msg.reply(f"{AddOhmsBot.msg_prefix}I couldn't do that at the moment. Sorry ☹️")
 
-        # Update the database
-        AddOhmsBot.MQTT.set_cooldown(mqtt_topic, new_cooldown)
+    @SubCommand(push_treat, "cooldown", permission="admin")
+    async def treatme_cooldown(self, msg, *args):
+        try:
+            new_cooldown = int(args[0])
 
-        await msg.reply(f"Treatme cooldown changed to {new_cooldown}")
+            # Update the database
+            AddOhmsBot.MQTT.set_cooldown(mqtt_topic, new_cooldown)
 
-    except IndexError:
-        # Happens if no arguments are presented
-        await msg.reply(f"Current cooldown is {AddOhmsBot.MQTT.get_cooldown(mqtt_topic)} seconds.")
-        return
+            await msg.reply(f"Treatme cooldown changed to {new_cooldown}")
 
-    except ValueError:
-        await msg.reply("Invalid value.")
+        except IndexError:
+            # Happens if no arguments are presented
+            await msg.reply(f"Current cooldown is {AddOhmsBot.MQTT.get_cooldown(mqtt_topic)} seconds.")
+            return
 
-    except Exception as e:
-        print(type(e), e)
-        return
+        except ValueError:
+            await msg.reply("Invalid value.")
+
+        except Exception as e:
+            print(type(e), e)
+            return
