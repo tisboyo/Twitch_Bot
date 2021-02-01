aefbfc3a621e912dae4d1fe6fe639a7de42dc274
tisboyo <10230718+tisboyo@users.noreply.github.com>
2021-02-01 03:27:12 +0000

    New treatbot handling for #72

--- bot/mods/treats.py
+++ bot/mods/treats.py
@@ -1,25 +1,54 @@
 from helpers.points import Points
+from helpers.points import Status
 from main import AddOhmsBot
 from twitchbot.command import ModCommand
 from twitchbot.command import SubCommand
 from twitchbot.enums import CommandContext
+from twitchbot.message import Message
 from twitchbot.modloader import Mod
 
 mqtt_topic = AddOhmsBot.MQTT.Topics.dispense_treat_toggle
+trigger_emoji = "balden3Ducky"
 
 
 class Treats(Mod):
-    name = "yay"
+    name = "treats"
 
     def __init__(self):
-        self.points = Points(emojis=5, unique_users=2, mod_commands=1, require_all=True, emoji_cap=3)
+        self.points = Points(emojis=5, unique_users=-1, mod_commands=1, require_all=True, emoji_cap=5)
+        self.reminder_enable = True
+        self.ready_for_trigger = True
+
+    async def on_raw_message(self, msg: Message):
+        # If the message is a system message, we're done here
+        if not msg.author or not msg.content:
+            return
+
+        emoji_count = msg.content.count(trigger_emoji)
+        if emoji_count > 0:
+            if self.points.check(msg, emojis=emoji_count):
+                await self.send_treat(msg)
+            else:  # Check how many more we need
+                status = self.points.status()
+
+                if status.emojis_required >= status.emojis and self.reminder_enable:
+                    await msg.reply(f"{AddOhmsBot.msg_prefix} {self.build_required_message(status)}")
+                    self.reminder_enable = False
+                elif status.emojis >= status.emojis_required:
+                    await msg.reply(f"{AddOhmsBot.msg_prefix} A treat is in the queue!!")
+
+        else:
+            # If it doesn't include a Yay, we don't care about it at all
+            self.reminder_enable = True  # Allow the reminder to be sent again
+            return
 
     @ModCommand(name, "treatme", context=CommandContext.BOTH, permission="admin")
     async def push_treat(self, msg, *args):
-        if await AddOhmsBot.MQTT.send(mqtt_topic):
-            await msg.reply(f"{AddOhmsBot.msg_prefix}Teleporting a treat")
+        if self.points.check(msg, mod_commands=1):
+            await self.send_treat(msg)
         else:
-            await msg.reply(f"{AddOhmsBot.msg_prefix}I couldn't do that at the moment. Sorry ☹️")
+            status = self.points.status()
+            await msg.reply(self.build_required_message(status))
 
     @SubCommand(push_treat, "cooldown", permission="admin")
     async def treatme_cooldown(self, msg, *args):
@@ -42,3 +71,23 @@ class Treats(Mod):
         except Exception as e:
             print(type(e), e)
             return
+
+    def build_required_message(self, status: Status) -> str:
+        """Build a message to send to chat with requirements to get a treat"""
+
+        req_users = self.points.unique_users_required - len(status.unique_users["emojis"])
+        # Build message to send to chat
+        required = f"{AddOhmsBot.msg_prefix}{status.emojis_required - status.emojis} more {trigger_emoji} "
+        if req_users <= self.points.unique_users_required and self.points.unique_users_required > 0:
+            required += f"from {req_users} more users "
+        required += "required for a treat!"
+
+        return required
+
+    async def send_treat(self, msg: Message) -> bool:
+        if await AddOhmsBot.MQTT.send(mqtt_topic, 1):
+            await msg.reply(f"{AddOhmsBot.msg_prefix}Treats!! balden3Yay balden3Yay balden3Yay")
+            return True
+        else:
+            await msg.reply(f"{AddOhmsBot.msg_prefix}I ran into a problem with MQTT. Sorry ☹️")
+            return False
