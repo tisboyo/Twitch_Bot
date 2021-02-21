0499987a45ac37e62f266261ae65b695f1312710
tisboyo <10230718+tisboyo@users.noreply.github.com>
2021-02-21 21:00:47 +0000

    Added user ignore check #124

--- bot/commands/mqtt_test.py
+++ bot/commands/mqtt_test.py
@@ -1,16 +1,20 @@
-from main import AddOhmsBot
+from main import bot
 from mqtt import MqttTopics
 from twitchbot import Command
+from twitchbot.message import Message
 
 
 @Command("mqtttest", permission="admin")
-async def mqtt_test(msg, *args):
+async def mqtt_test(msg: Message, *args):
+    if bot.user_ignored(str(msg.author)):
+        return
+
     # Build the message
     message = ""
     for arg in args:
         message += f"{arg} "
 
-    if await AddOhmsBot.MQTT.send(MqttTopics.test, message):
-        await msg.reply(f"{AddOhmsBot.msg_prefix} Connection successful.")
+    if await bot.MQTT.send(MqttTopics.test, message):
+        await msg.reply(f"{bot.msg_prefix} Connection successful.")
     else:
-        await msg.reply(f"{AddOhmsBot.msg_prefix} Connection error.")
+        await msg.reply(f"{bot.msg_prefix} Connection error.")
