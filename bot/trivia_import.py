c1fba4e6aab2ef650a34c24c69092bedd3b407af
tisboyo <10230718+tisboyo@users.noreply.github.com>
2021-03-22 18:54:23 +0000

    Working trivia_import (and update) #151

--- bot/trivia_import.py
+++ bot/trivia_import.py
@@ -1,5 +1,7 @@
+# Created for #151
 import json
-from datetime import date
+from datetime import datetime
+from typing import Union
 
 from mods._database import session
 
@@ -11,28 +13,54 @@ with open("questions.txt") as file:
     questions = questions["quiz"]
 
 
-for key, values in questions.items():
-    query_result = session.query(Trivia).filter(Trivia.id == int(key)).one_or_none()
+for id_num, values in questions.items():
+    try:
+        # Format the question data so the database can handle it.
+        question_data: dict = dict(  # Intentionally excluding id from this to prevent changing it accidentally
+            text=values["text"],
+            answers=json.dumps(values["answers"]),
+            explain=values["explain"],
+            last_used_date=datetime.fromisoformat(values["last_used_date"])
+            if values["last_used_date"] != ""
+            else datetime.min,
+            last_update_date=datetime.today(),
+            created_date=datetime.fromisoformat(values["created_date"]) if values["created_date"] != "" else datetime.min,
+            created_by=values["created_by"],
+            reference=values["reference"],
+        )
+    except KeyError as e:
+        print(f"Missing value for {e.args[0]} for question #{id_num}")
+        continue
+
+    # Read the current database for this id
+    query_result: Union[Trivia, None] = session.query(Trivia).filter_by(id=int(id_num)).one_or_none()
+
     if query_result is None:
-        try:
-            insert = Trivia(
-                id=int(key),
-                text=values["text"],
-                answers=json.dumps(values["answers"]),
-                explain=values["explain"],
-                last_used_date=date.fromisoformat(values["last_used_date"]) if values["last_used_date"] != "" else date.min,
-                created_date=date.fromisoformat(values["created_date"]) if values["created_date"] != "" else date.min,
-                created_by=values["created_by"],
-                reference=values["reference"],
-            )
-            session.add(insert)
+        # Insert the question into the database
+        insert = Trivia(id=int(id_num), **question_data)
+        session.add(insert)
+        session.commit()
+        print(f"Trivia question #{id_num} inserted.")
+
+    elif query_result.text != values["text"] and not values.get("force_update", False):
+        # Output a message if question text does not match and force_update is not True
+        print(f"Question #{id_num} text does not match database. Not updating it. Add force_update=true to force.")
+
+    else:
+        # Check if force update flag is set, if so warn the console
+        if values.get("force_update", False):
+            print(f"Question #{id_num} is set to force update. You should remove that.")
+
+        if (  # Update the questions data if it has changed
+            values.get("force_update", False)
+            or query_result.answers != question_data["answers"]
+            or query_result.last_used_date != question_data["last_used_date"]
+            or query_result.reference != question_data["reference"]
+            or query_result.explain != question_data["explain"]
+        ):
+            results = session.query(Trivia).filter_by(id=id_num).update(question_data)
             session.commit()
-            print(f"Trivia question #{key} inserted.")
+            print(f"Question #{id_num} updated.")
 
-        except KeyError as e:
-            print(f"Missing value for {e.args[0]}")
-
-    elif query_result.text != values["text"]:
-        print(f"Question #{key} question text does not match database.")
 
 print("Trivia update complete...")
