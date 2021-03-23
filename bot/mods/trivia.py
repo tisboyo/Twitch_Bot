6b47e2f739b081d6a25240ac710130cb5ce14f64
tisboyo <10230718+tisboyo@users.noreply.github.com>
2021-03-23 13:32:57 +0000

    152

--- bot/mods/trivia.py
+++ bot/mods/trivia.py
@@ -1,1 +1,28 @@
 # Added for #152
+from main import bot
+from mods._database import session
+from twitchbot import cfg
+from twitchbot import Channel
+from twitchbot import channels
+from twitchbot import CommandContext
+from twitchbot import Mod
+from twitchbot import ModCommand
+from twitchbot import SubCommand
+
+from models import Settings
+
+
+class TriviaMod(Mod):
+    name = "triviamod"
+
+    def __init__(self):
+        pass
+
+    @ModCommand(name, "trivia", permission="admin")
+    async def trivia(self, msg, *args):
+        pass
+
+    @SubCommand(trivia, "q")
+    async def trivia_q(self, msg, *args):
+        print(args)
+        pass
