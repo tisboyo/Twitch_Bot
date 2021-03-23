fa3bbb41485f789b8f7caa8d793f69f0d35be5ed
tisboyo <10230718+tisboyo@users.noreply.github.com>
2021-03-23 18:36:30 +0000

    WIP #152

--- bot/mods/trivia.py
+++ bot/mods/trivia.py
@@ -8,21 +8,42 @@ from twitchbot import CommandContext
 from twitchbot import Mod
 from twitchbot import ModCommand
 from twitchbot import SubCommand
-
+from twitchbot.message import Message
+from asyncio import sleep
 from models import Settings
+from models import TriviaQuestions
 
 
 class TriviaMod(Mod):
     name = "triviamod"
 
     def __init__(self):
-        pass
+        super().__init__()
+        self.current_question: int = -1
+        self.current_answer: int = -1
+        self.current_participants: dict = dict()  # {username:(correct, incorrect)}
+        self.msg_prefix: str = "❔"
 
     @ModCommand(name, "trivia", permission="admin")
     async def trivia(self, msg, *args):
-        pass
+        print("trivia")
 
-    @SubCommand(trivia, "q")
-    async def trivia_q(self, msg, *args):
-        print(args)
-        pass
+    # This command will almost always be sent by the webserver,
+    # setting the CommandContext to Whisper will allow it in whispers
+    # only, but when sent over the command console, context is ignored.
+    @SubCommand(trivia, "q")  # , context=CommandContext.WHISPER) #TODO
+    async def trivia_q(self, msg: Message, question_num: str, answer_num: str):
+        question_num, answer_num = int(question_num), int(answer_num)
+        self.current_question = question_num
+        self.current_answer = answer_num
+
+        result = session.query(TriviaQuestions).filter_by(id=self.current_question).one_or_none()
+
+        if result is not None:
+            await msg.reply(f"{self.msg_prefix}{result.text}")
+            await sleep(10)
+            await msg.reply(f"{self.msg_prefix}15 seconds remaining")
+            await sleep(10)
+            await msg.reply(f"{self.msg_prefix}Final answers...")
+            await sleep(5)
+            await msg.reply(f"{self.msg_prefix}Answer: A")
