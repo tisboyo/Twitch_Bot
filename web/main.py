157b6a21c9e3d28e531c9e4bf3a4b8399bf13f2e
tisboyo <10230718+tisboyo@users.noreply.github.com>
2021-04-04 23:02:43 +0000

    Change to route imports

--- web/main.py
+++ web/main.py
@@ -13,16 +13,17 @@ from models import Settings
 
 # noreorder
 # Routes
-from routes import announcements
-from routes import commands
-from routes import docs
-from routes import dropbox
-from routes import poll_display
-from routes import send_command
-from routes import send_message
-from routes import topic
-from routes import twitch_webhook
-from routes import ignore
+import routes.announcements
+import routes.commands
+import routes.docs
+import routes.dropbox
+import routes.poll_display
+import routes.send_command
+import routes.send_message
+import routes.topic
+import routes.twitch_webhook
+import routes.ignore
+import routes.trivia
 
 app = FastAPI(docs_url=None, redoc_url=None, openapi_url=None)
 
@@ -34,16 +35,17 @@ db_url = f"mysql+mysqlconnector://{mysql_user}:{mysql_pass}@mysql:3306/{mysql_da
 app.add_middleware(DBSessionMiddleware, db_url=db_url)
 
 # Include the routes
-app.include_router(announcements.router)
-app.include_router(twitch_webhook.router)
-app.include_router(send_message.router)
-app.include_router(docs.router)
-app.include_router(commands.router)
-app.include_router(poll_display.router)
-app.include_router(dropbox.router)
-app.include_router(topic.router)
-app.include_router(send_command.router)
-app.include_router(ignore.router)
+app.include_router(routes.announcements.router)
+app.include_router(routes.twitch_webhook.router)
+app.include_router(routes.send_message.router)
+app.include_router(routes.docs.router)
+app.include_router(routes.commands.router)
+app.include_router(routes.poll_display.router)
+app.include_router(routes.dropbox.router)
+app.include_router(routes.topic.router)
+app.include_router(routes.send_command.router)
+app.include_router(routes.ignore.router)
+app.include_router(routes.trivia.router)
 
 
 @app.on_event("startup")
