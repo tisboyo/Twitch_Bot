df662545c8c89612bf656644e9127cccb659012e
tisboyo <10230718+tisboyo@users.noreply.github.com>
2021-04-04 23:42:48 +0000

    Add /trivia page #152

--- web/routes/trivia.py
+++ web/routes/trivia.py
@@ -1,8 +1,14 @@
-from os import getenv
-
 from fastapi import APIRouter
-from fastapi import Request
-from fastapi.responses import FileResponse
-from starlette.responses import RedirectResponse
+from fastapi.responses import Response
+from send_to_bot import send_command_to_bot
 
 router = APIRouter()
+
+
+@router.get("/trivia")
+async def trivia(question: int = -1, answer: int = -1):
+    if question == -1 or answer == -1:
+        return Response(status_code=418)
+
+    ret = await send_command_to_bot("trivia", ["q", question, answer])
+    return Response(status_code=ret.status_code)
