05f5786eeda889c4d19035a30324eccd5ca600c2
tisboyo <10230718+tisboyo@users.noreply.github.com>
2021-04-10 18:54:18 +0000

    Start work on integrating trivia into webserver #152

--- web/routes/trivia.py
+++ web/routes/trivia.py
@@ -1,19 +1,96 @@
+from os import getenv
+
 from fastapi import APIRouter
+from fastapi.exceptions import HTTPException
 from fastapi.responses import Response
+from fastapi.templating import Jinja2Templates
 from send_to_bot import send_command_to_bot
+from starlette.requests import Request
+from starlette.responses import FileResponse
+from starlette.responses import JSONResponse
+
 
 router = APIRouter()
 
-
-@router.get("/trivia")
-async def trivia(question: int = -1, answer: int = -1):
-    if question == -1 or answer == -1:
-        return Response(status_code=418)
-
-    ret = await send_command_to_bot("trivia", ["q", question, answer])
-    return Response(status_code=ret.status_code)
+web_api_key = getenv("WEB_API_KEY")
+templates = Jinja2Templates(directory="static_files/trivia")
 
 
-@router.post("/endtrivia")
-async def end_trivia():
+@router.get("/trivia/q")
+async def trivia_q(request: Request, key: str = None):
+    if key != web_api_key:
+        return Response(status_code=403)
+
+    jsonret = {
+        "text": "What fictional device enables time travel?",
+        "answers": {
+            "4": {"text": "Warp Core", "is_answer": 0},
+            "1": {"text": "Flux Capacitor", "is_answer": 1},
+            "3": {"text": "Reluctance Inductor", "is_answer": 0},
+            "2": {"text": "A Key", "is_answer": 0},
+        },
+        "explain": "Back to the Future established that with 1.21 GW the Flux Capacitor is what makes time travel possible.",
+        "last_used_date": "",
+        "created_date": "2019-11-24",
+        "created_by": "baldengineer",
+        "reference": "",
+    }
+
+    question = 16
+    answer = 2
+
+    try:
+        await send_command_to_bot("trivia", ["q", question, answer])
+    except HTTPException:
+        return JSONResponse(
+            {
+                "text": "Unable to connect to TwitchBot for Trivia",
+                "answers": {
+                    "1": {"text": "Fire Tisboyo."},
+                    "2": {"text": "Sacrifice some Ohms to the bot gods."},
+                    "3": {"text": "Kick AWS for breaking the bot."},
+                    "4": {"text": "Did the Twitch API key expire again?"},
+                },
+                "explain": "Lets try again and see if it wakes up this time.",
+            }
+        )
+
+    return JSONResponse(jsonret)
+
+
+@router.post("/trivia/end")
+async def trivia_end(key: str = None):
+    if key != web_api_key:
+        return Response(status_code=403)
+
     print("end trivia")
+
+
+# Static file returns
+@router.get("/trivia")
+async def trivia_index(request: Request, key: str = None):
+    if key != web_api_key:
+        return Response(status_code=403)
+
+    return templates.TemplateResponse("index.html", {"request": request, "key": key})
+
+
+@router.get("/trivia/trivia.js")
+async def trivia_js(request: Request, key: str = None):
+    if key != web_api_key:
+        return Response(status_code=403)
+
+    return FileResponse("static_files/trivia/trivia.js")
+
+
+@router.get("/trivia/trivia.css")
+async def trivia_css(request: Request):
+    return FileResponse("static_files/trivia/trivia.css")
+
+
+@router.get("/trivia/laptop-background-transparent.png")
+async def trivia_background(request: Request, key: str = None):
+    if key != web_api_key:
+        return Response(status_code=403)
+
+    return FileResponse("static_files/trivia/laptop-background-transparent.png")
---
+++ web/static_files/trivia/trivia.js
@@ -0,0 +1,171 @@
+//Global Variables
+setting_QuestionCountDown = 30;
+setting_AnswerCountDown = 10;
+countDownSeconds = setting_QuestionCountDown;
+
+var questionOBJ;
+
+//Grab the API Key and build URL for requesting questions
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString);
+const api_key = urlParams.get('key')
+yourUrl = "/trivia/q?key=" + api_key;
+
+// Global Timers
+var displayAnswerTimer;
+var myTimer;
+
+var endTriviaNotified = false
+document.addEventListener("visibilitychange", function () {
+    if (document.visibilityState === 'hidden') {
+        if (!endTriviaNotified) {
+            endTriviaNotified = true;
+            navigator.sendBeacon('/trivia/end?key=' + api_key);
+        }
+    }
+});
+
+function displayAnswer() {
+    countDownSeconds--;
+    document.getElementById("timer").innerHTML = "<h1>" + countDownSeconds + "</h1>";
+    if (countDownSeconds <= 0) {
+        clearInterval(displayAnswerTimer);
+        console.log("time for the next question");
+        document.getElementById("explain").style.visibility = "hidden";
+        //document.getElementById("timer").style.visibility = "visible";
+        if (document.visibilityState === 'visible') {
+            location.reload();
+        }
+
+        //getAQuestion();
+    }
+}
+
+function countDownTimer() {
+    countDownSeconds--;
+    //    console.log(countDownSeconds);
+    document.getElementById("timer").innerHTML = "<h1>" + countDownSeconds + "</h1>";
+    if (countDownSeconds <= 0) {
+        console.log("Question timer expired");
+        clearInterval(myTimer);
+
+        // setup Answer
+        countDownSeconds = setting_AnswerCountDown;
+        displayAnswerTimer = setInterval(displayAnswer, 1000);
+        document.getElementById("explain").style.visibility = "visible";
+        // document.getElementById("timer").style.visibility = "hidden";
+        document.getElementById("correct-answer").className = "correct-answer";
+        // the_answer.id="correct-answer";
+        //the_answer.className = "correct-answer";
+    }
+}
+
+function Get(yourUrl) {
+    var Httpreq = new XMLHttpRequest(); // a new request
+    Httpreq.open("GET", yourUrl, false);
+    Httpreq.send(null);
+    return Httpreq.responseText;
+}
+
+function shuffle(array) {
+    //Shuffling moved to python script.
+    // var currentIndex = array.length, temporaryValue, randomIndex;
+
+    // // While there remain elements to shuffle...
+    // while (0 !== currentIndex) {
+
+    //   // Pick a remaining element...
+    //   randomIndex = Math.floor(Math.random() * currentIndex);
+    //   currentIndex -= 1;
+
+    //   // And swap it with the current element.
+    //   temporaryValue = array[currentIndex];
+    //   array[currentIndex] = array[randomIndex];
+    //   array[randomIndex] = temporaryValue;
+    // }
+
+    return array;
+}
+
+function processResponse(obj) {
+    // the question
+    document.getElementById("question").innerHTML = "<h2>" + obj.text + "</h2>";
+
+    // clear the questions
+    document.getElementById("answers").innerHTML = "";
+
+    // the explanation
+    document.getElementById("explain").innerHTML = obj.explain;
+    document.getElementById("explain").style.visibility = "hidden";
+
+    // answers
+    var answerOBJs = obj.answers;
+
+    // generate an array of keys and then randomize them
+    // so the answers come out "random"
+    var answer_objs_key_count = Object.keys(answerOBJs).length;
+    var answer_key_order = [];
+    console.log("Length of answer objs: " + answer_objs_key_count);
+    for (x = 0; x < answer_objs_key_count; x++)
+        answer_key_order[x] = x;
+    console.log("Going to check these: " + answer_key_order.toString());
+    var random_key_order = shuffle(answer_key_order);
+    console.log("Now we got: " + random_key_order.toString());
+
+    //for (var answer_key of Object.keys(answerOBJs)) {
+    var answer_letter_counter = 65;
+    for (this_random_key = 0; this_random_key < answer_objs_key_count; this_random_key++) {
+        var answer_key = random_key_order[this_random_key] + 1;
+        var answer_text = answerOBJs[answer_key].text;
+        if (answerOBJs[answer_key].is_answer == '1')
+            var is_answer = 1;
+        else
+            var is_answer = 0;
+        //console.log(answer_key);
+        //console.log(answer_text);
+        //console.log(is_answer);
+
+        // convert 1-x to letters
+        var answer_letter = String.fromCharCode(answer_letter_counter++);
+
+        // generate the HTML
+        var answer_section = document.createElement("div");
+        var the_answer = document.createElement("h2");
+        var answer_node = document.createTextNode(answer_letter + ": " + answer_text);
+        answer_section.id = "answer";
+
+        // need to re-think how to do this... maybe reload the text
+        // and have a flag?
+        if (is_answer)
+            the_answer.id = "correct-answer";
+        //the_answer.className = "correct-answer";
+
+        answer_section.appendChild(the_answer).appendChild(answer_node);
+        document.getElementById("answers").appendChild(answer_section);
+    }
+}
+
+function questionTimerStart() {
+    // reset the clock
+    countDownSeconds = setting_QuestionCountDown;
+    myTimer = setInterval(countDownTimer, 1000);
+}
+
+function getAQuestion() {
+    console.log("Getting a question");
+    //are we on the pi or are we local?
+    if (yourUrl == "") {
+        console.log("Looks like we are local...");
+        //var exampleJSON = '{"text":"Who invented the transistor?", "answers": {"1": {"text": "William Shockley", "is_answer": 1}, "2": {"text": "Alexander Bell", "is_answer": 0}, "3": {"text": "Daniel Bernoulli", "is_answer": 0}, "4": {"text": "Michael Jacob", "is_answer": 0}}, "explain": "Shockley, Bardeen, and Brattain were scientists who discovered the transistor effect at Bell Labs.", "last_used_date": "", "created_date": "2019-11-24", "created_by": "baldengineer", "reference": "https://en.wikipedia.org/wiki/William_Shockley"}';
+        var exampleJSON = '{"text": "Radio Direction Ranging is also known as:", "answers": {"1": {"text": "LiDAR", "is_answer": 0}, "2": {"text": "RADAR", "is_answer": 1}, "3": {"text": "Foxing", "is_answer": 0}, "4": {"text": "Futzing", "is_answer": 0}}, "explain": "The first range finding experiment occured in 1924.", "last_used_date": "", "created_date": "2019-11-24", "created_by": "baldengineer", "reference": "http://www.ob-ultrasound.net/radar.html"}';
+        questionOBJ = JSON.parse(exampleJSON);
+    } else {
+        console.log("Looks like we are on the Pi... Mmmmm Pie...");
+        var response = Get(yourUrl);
+        console.log(response);
+        questionOBJ = JSON.parse(response);
+    }
+
+    processResponse(questionOBJ);
+    questionTimerStart();
+}
