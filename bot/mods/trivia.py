b078b77cc17e4673e687409825d85c7fae4acc19
tisboyo <10230718+tisboyo@users.noreply.github.com>
2021-07-31 20:59:55 +0000

    Tweak 15 second warning message #220

--- bot/mods/trivia.py
+++ bot/mods/trivia.py
@@ -111,7 +111,7 @@ class TriviaMod(Mod):
                 await msg.reply(f"{self.msg_prefix}{result.text}")
 
             if await sleep_if_active(45):
-                await msg.reply(f"{self.msg_prefix}15 seconds remaining")
+                await msg.reply(f"{self.msg_prefix}Time is almost up")
 
             if await sleep_if_active(10):
                 await msg.reply(f"{self.msg_prefix}Final answers...")
