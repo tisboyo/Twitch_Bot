3642707a96533203413894e0331becd6b218f560
tisboyo <10230718+tisboyo@users.noreply.github.com>
2021-08-04 01:42:39 +0000

    Disable auto import on startup #151

--- bot/start.sh
+++ bot/start.sh
@@ -3,8 +3,8 @@ echo "Updating database..."
 python alembic_update.py
 
 #Added in #151
-echo "Updating Trivia..."
-python trivia_import.py
+# echo "Updating Trivia..."
+# python trivia_import.py
 
 echo "Starting Bot..."
 python main.py
