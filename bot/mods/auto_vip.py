4d165b4b757027564090a6b207fe41da861507ec
tisboyo <10230718+tisboyo@users.noreply.github.com>
2021-08-08 17:27:07 +0000

    Auto-add Twitch VIP's to Bot's vip list Fixes #236

---
+++ bot/mods/auto_vip.py
@@ -0,0 +1,49 @@
+from twitchbot import cfg
+from twitchbot import Channel
+from twitchbot import Mod
+from twitchbot import ModCommand
+from twitchbot.enums import MessageType
+from twitchbot.message import Message
+from twitchbot.permission import perms
+
+
+class AutoVIPMod(Mod):
+    name = "autovipmod"
+
+    def __init__(self):
+        super().__init__()
+        self.vip_group = "vip"
+        print("AutoVIP loaded")
+
+    @ModCommand(name, "vipupdate", permission="admin")
+    async def vip_update(self, msg: Message, channel: Channel, *args):
+        await channel.send_message("/vips")
+
+    async def on_channel_joined(self, channel: Channel):
+        await channel.send_message("/vips")
+
+    async def on_raw_message(self, msg: Message):
+        if msg.type is MessageType.NOTICE and msg.msg_id == "vips_success":
+            # Split the message on the colon which removes the preamble
+            temp_parse = msg.normalized_content.split(":", maxsplit=1)
+            # We only care about the userlist, overwrite with just that
+            temp_parse = temp_parse[1]
+            # strip off the leading space and trailing period
+            temp_parse = temp_parse[:-1].strip()
+            # split them into a list
+            vip_users = temp_parse.split(", ")
+
+            channel = cfg.channels[0]
+            vip_group: dict = perms.get_group(channel, self.vip_group)
+
+            # For users that are twitch vips add them to the bot vip group
+            for user in vip_users:
+                if user not in vip_group["members"]:
+                    perms.add_member(channel, self.vip_group, user)
+                    print(f"{user} was added to bot vip group.")
+
+            # Check if there are users in the bot's vip list and notify about/remove them
+            for user in vip_group["members"]:
+                if user not in vip_users:
+                    perms.delete_member(channel, self.vip_group, user)
+                    print(f"{user} was removed from the bot vip group.")
