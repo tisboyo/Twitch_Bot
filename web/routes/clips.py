e18838785d1730ad0b966408bec443aa5a4bdae1
tisboyo <10230718+tisboyo@users.noreply.github.com>
2021-08-12 23:34:13 +0000

    Add clip add functionality #221

--- web/routes/clips.py
+++ web/routes/clips.py
@@ -1,3 +1,5 @@
+import re
+
 from fastapi import APIRouter
 from fastapi import Request
 from fastapi.params import Form
@@ -56,15 +58,22 @@ async def get_clips(request: Request):
                 xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
                 xhttp.send("clip_id="+clip_id+"&enable="+enable);
             }
-        </script>
+        </script>"""
+    out += f"""
         <table border=1>
-        <tr><td colspan=5 align=center><div id="status">Manage Clips</div></td></tr>
+        <tr><td colspan=10 align=center>
+            <div id="status">
+        {request.cookies.get('clipmsg') if request.cookies.get('clipmsg') else 'Manage Clips.'}
+            </div>
+            </td>
+        </tr>
             <tr>
                 <td>ID</td>
                 <td>Enabled</td>
                 <td>Name</td>
                 <td>Title</td>
                 <td>URL</td>
+                <td>Added By</td>
                 <td>Delete</td>
             </tr>"""
     for clip in result:
@@ -81,20 +90,45 @@ async def get_clips(request: Request):
                 <td>{clip.name}</td>
                 <td>{clip.title}</td>
                 <td><a href=https://clips.twitch.tv/{clip.url} target=_blank>{clip.url}</a></td>
+                <td>{clip.added_by}</td>
                 <td><form action="/clips/delete" method=POST>
                     <button name="clip_id" value={clip.id} onclick="return confirm('Do you really want to delete this clip?');">🗑️</button>
                     </form>
                 </td>
             </tr>
+        """
+
+    out += """
+        </table>
+        <p>
+        <form method=POST action="/clips/add">
+        <table border=1>
+            <tr><td colspan=10 align=center>Insert new Clip</td></tr>
+            <tr>
+                <td>Enabled</td>
+                <td>Name</td>
+                <td>Title</td>
+                <td>URL</td>
+            </tr>
+            <tr>
+                <td><input type="checkbox" name="enabled"></td>
+                <td><input type="text" name="name"></td>
+                <td><input type="text" name="title"></td>
+                <td><input type="text" name="url"></td>
+                <td><input type="submit" value="Save"></td>
+            </tr>
         </table>
     </body>
     </html>"""
-    return out
+
+    response = HTMLResponse(out)
+    response.set_cookie("clipmsg", "")
+    return response
 
 
 @router.get("/clips/json")
 async def get_clips_json(request: Request):
-    result = db.session.query(Clips).order_by(Clips.id).all()
+    result = db.session.query(Clips).filter(Clips.enabled == True).order_by(Clips.id).all()  # noqa E712
     return result
 
 
@@ -107,8 +141,10 @@ async def post_clips_delete(request: Request, clip_id: int = Form(...)):
         db.session.query(Clips).filter(Clips.id == clip_id).delete()
         db.session.commit()
 
-        print(f"Deleted clip id:{clip_id}")
-        return RedirectResponse("/clips", status_code=303)
+        print(f"{me.username} Deleted clip id:{clip_id}")
+        response = RedirectResponse("/clips", status_code=303)
+        response.set_cookie("clipmsg", f"Deleted clip id:{clip_id}")
+        return response
     else:
         raise HTTPException(403)
 
@@ -131,3 +167,47 @@ async def post_clips_enabled(request: Request, clip_id: int = Form(...), enable:
     else:
         # User wasn't an admin or mod
         raise HTTPException(403)
+
+
+@router.post("/clips/add")
+async def post_clips_add(
+    request: Request,
+    enabled: bool = Form(default=False),
+    name: str = Form(...),
+    title: str = Form(default=""),
+    url: str = Form(...),
+):
+    try:
+        # Ensure logged in user
+        check_user_valid(request)
+        me = get_user(request)
+
+    except HTTPException:
+        # Redirect to login if not
+        response = RedirectResponse("/login")
+        response.set_cookie(key="redirect", value=request.url.path)
+        return response
+
+    regex = r"(https?:\/\/(www\.|clips\.)?twitch\.tv\/(baldengineer\/clip\/)?(?P<clip_id>[A-Za-z0-9]*)?)"
+    match = re.search(regex, url)
+
+    # Define this up here so we can set the clipmsg cookie throughout
+    response = RedirectResponse("/clips", status_code=303)
+
+    if match and match.group("clip_id"):  # Only query if the url was valid and has a clip id
+        clip_id = match.group("clip_id")
+
+        query = db.session.query(Clips).filter(Clips.name == name.lower()).one_or_none()
+        if not query:
+            # name didn't exist, so it's time to add it
+            clip = Clips(name=name.lower(), enabled=enabled, title=title, url=clip_id, added_by=me.username)
+
+            db.session.add(clip)
+            db.session.commit()
+            response.set_cookie("clipmsg", f"{name} has been added to the clip database.")
+        else:
+            response.set_cookie("clipmsg", f"Sorry, {name} already exists in the clip database.")
+    else:
+        response.set_cookie("clipmsg", "Sorry, that's not a valid clip url.")
+
+    return response
