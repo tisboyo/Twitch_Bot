58dff06273d01adbda14e3ec1ce683fa2902fab4
tisboyo <10230718+tisboyo@users.noreply.github.com>
2021-09-25 00:59:03 +0000

    Move message received inside treat check

--- bot/temp_bridge.py
+++ bot/temp_bridge.py
@@ -60,9 +60,9 @@ class Paho:
         self.port = port
 
     def on_message(self, client, userdata, message):
-        print("message received  ", str(message.payload.decode("utf-8")))
         topic = message.topic.split("/")
         if topic[1] == "dispense-treat-toggle":
+            print("message received  ", str(message.payload.decode("utf-8")))
             self.AIO.send(topic[1], message.payload.decode("utf-8"))
 
     def on_connect(self, client, userdata, flags, rc):
