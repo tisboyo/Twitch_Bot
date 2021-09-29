7995c46ef5b6c7983663dcbf097bdbb9f3a42323
tisboyo <10230718+tisboyo@users.noreply.github.com>
2021-09-29 03:25:18 +0000

    Add bot regex manage pages #267

--- web/routes/auto_bahn.py
+++ web/routes/auto_bahn.py
@@ -1,6 +1,10 @@
 from fastapi import APIRouter
 from fastapi import Request
+from fastapi.exceptions import HTTPException
 from fastapi.params import Depends
+from fastapi.params import Form
+from fastapi.responses import FileResponse
+from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from fastapi_sqlalchemy import db
 from starlette.responses import RedirectResponse
@@ -22,9 +26,55 @@ async def autoban(request: Request, user=Depends(check_user(level=AuthLevel.admi
     return RedirectResponse("/autoban/manage")
 
 
-@router.get("/autoban/manage")
+@router.get("/autoban/manage/")
 async def autoban_manage(request: Request, user=Depends(check_user(level=AuthLevel.admin))):
+    return RedirectResponse("/autoban/manage.html")
+
+
+@router.get("/autoban/manage.html")
+async def autoban_manage_html(request: Request, user=Depends(check_user(level=AuthLevel.admin))):
 
     result = db.session.query(BotRegex).order_by(BotRegex.id).all()
 
-    return templates.TemplateResponse("manage.html.jinja", {"request": request, "result": result})
+    response = templates.TemplateResponse("manage.html.jinja", {"request": request, "result": result})
+    response.set_cookie("msg", "")
+    return response
+
+
+@router.get("/autoban/manage.js")
+async def autoban_manage_js(request: Request, user=Depends(check_user(level=AuthLevel.admin))):
+
+    return FileResponse("static_files/autoban/manage.js")
+
+
+@router.post("/autoban/manage/enable")
+async def autoban_manage_enable(
+    request: Request,
+    regex_id: int = Form(...),
+    enable: bool = Form(...),
+    user=Depends(check_user(level=AuthLevel.admin)),
+):
+    query = db.session.query(BotRegex).filter(BotRegex.id == regex_id).one_or_none()
+    if query:
+        query.enabled = enable
+        db.session.commit()
+        return HTMLResponse(f"BotRegex #{query.id} {'enabled' if query.enabled else 'disabled'}.")
+    else:
+        # Announcement ID wasn't in database
+        raise HTTPException(404)
+
+
+@router.post("/autoban/manage/delete")
+async def autoban_manage_delete(
+    request: Request, regex_id: int = Form(...), user=Depends(check_user(level=AuthLevel.admin))
+):
+    query = db.session.query(BotRegex).filter(BotRegex.id == regex_id).one_or_none()
+
+    if query:
+        db.session.query(BotRegex).filter(BotRegex.id == regex_id).delete
+        db.session.commit()
+        print(f"{user.username} Deleted BotRegEx: {query.pattern}")
+        return HTMLResponse(f"Deleted: {query.pattern}")
+    else:
+        # Announcement ID wasn't in database
+        raise HTTPException(404)
