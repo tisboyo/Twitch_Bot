3ce13ea51b006112b236b1e5a5661b098e125509
tisboyo <10230718+tisboyo@users.noreply.github.com>
2021-10-02 02:12:01 +0000

    Add post to server and saving to database #291

--- web/routes/discord_webhooks.py
+++ web/routes/discord_webhooks.py
@@ -1,6 +1,8 @@
+import aiohttp
 from fastapi import APIRouter
 from fastapi import Request
 from fastapi.params import Depends
+from fastapi.params import Form
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from fastapi_sqlalchemy import db
@@ -34,3 +36,29 @@ def discord_webhook_html(request: Request, user=Depends(check_user(level=AuthLev
 @router.get("/discord/webhook_manage.js", response_class=FileResponse)
 def discord_webhook_js(request: Request, user=Depends(check_user(level=AuthLevel.admin))):
     return FileResponse("static_files/discord/webhook_manage.js")
+
+
+@router.post("/discord/webhook_manage/save", response_class=HTMLResponse)
+async def discord_webhook_manage_save(
+    request: Request,
+    webhook_name: str = Form(...),
+    webhook_url: str = Form(...),
+    user=Depends(check_user(level=AuthLevel.admin)),
+):
+    print(webhook_name, webhook_url)
+
+    async with aiohttp.ClientSession() as session:
+        response = await session.post(
+            webhook_url,
+            json={"content": "Testing webhook address", "username": f"TwitchBot: Webhook Test by {user.username}"},
+        )
+        if response.status == 204:
+            result = db.session.query(Settings).filter(Settings.key == webhook_name).update({Settings.value: webhook_url})
+            db.session.commit()
+            if result:
+                return "Updated"
+            else:
+                return "Database update error."
+
+        else:
+            return f"Error: {response.reason} {response.status}"
--- web/static_files/discord/webhook_manage.js
+++ web/static_files/discord/webhook_manage.js
@@ -17,4 +17,15 @@ function saveUrlEdit(setting_name) {
     //TODO Setting the onclick isn't actually working correctly here for some reason, but it's not a show stopper
     //entry_div.onclick = function () { editUrl(setting_name, textbox.value); }
     entry_div.innerText = textbox.value;
+
+    var xhttp;
+    xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            document.getElementById("status").innerHTML = this.responseText;
+        }
+    };
+    xhttp.open("POST", "/discord/webhook_manage/save", true);
+    xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xhttp.send("webhook_name=" + setting_name + "&webhook_url=" + textbox.value);
 }
