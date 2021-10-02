9108c876a48c281e0ca9e67e7e9aa97fd3637b7b
tisboyo <10230718+tisboyo@users.noreply.github.com>
2021-10-02 01:34:53 +0000

    Changes to functions for saving changes to webhooks #291

--- web/static_files/discord/webhook_manage.js
+++ web/static_files/discord/webhook_manage.js
@@ -2,16 +2,19 @@ function editUrl(setting_name, url) {
     var setting = document.getElementById("url_" + setting_name);
     setting.onclick = "";
     setting.innerHTML = `
-    <form>
-        <input size=120 type="text" id="edit_${setting_name}" value="${url}">
+    <form action="">
+        <input size=120 type="text" id="edit_${setting_name}" value="${url}"></input>
+        <button type="button" onclick="saveUrlEdit('${setting_name}');">💾</button>
     </form >
     `;
-    //document.getElementById('edit_' + setting_name).onblur = saveUrlEdit(setting_name, function () { document.getElementById('edit_' + setting_name).value; });
 }
 
-function saveUrlEdit(setting_name, url) {
-    document.getElementById('status').innerHTML = 'updated ' + setting_name;
-    var setting = document.getElementById("url_" + setting_name);
-    setting.onclick = `editUrl('${setting_name}', '${url}')`;
-    setting.innerHTML = url
+function saveUrlEdit(setting_name) {
+    //document.getElementById('status').innerHTML = 'updated ' + setting_name;
+    var entry_div = document.getElementById('url_' + setting_name);
+    var textbox = document.getElementById("edit_" + setting_name);
+
+    //TODO Setting the onclick isn't actually working correctly here for some reason, but it's not a show stopper
+    //entry_div.onclick = function () { editUrl(setting_name, textbox.value); }
+    entry_div.innerText = textbox.value;
 }
