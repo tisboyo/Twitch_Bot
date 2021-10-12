6bd397e2ed240e7e0716e767d077f8f65d3aac96
tisboyo <10230718+tisboyo@users.noreply.github.com>
2021-10-12 22:51:16 +0000

    Change database keep alive loop to re-read irc token #298

--- bot/mods/_database.py
+++ bot/mods/_database.py
@@ -1,5 +1,10 @@
 from asyncio import sleep
+from datetime import datetime
+from datetime import timedelta
+from json import loads
+from os import environ
 
+import aiohttp
 from sqlalchemy import create_engine
 from sqlalchemy import orm
 from twitchbot import Mod
@@ -36,8 +41,42 @@ session = Database().session
 
 
 async def keep_alive_loop():
+    thirty_day_warning_sent = False
     while True:
-        session.query(Settings).filter(Settings.key == "keepalive").one_or_none()
+        # Replace the keep alive query with refreshing the twitch irc token in environment variable
+        # so if it changes, while the connection is active, the bot will use the new key for api queries
+        query = session.query(Settings.value).filter(Settings.key == "twitch_irc_token").one_or_none()
+        session.commit()
+        if query:
+            values = loads(query[0])
+            environ["oauth"] = values["access_token"]
+
+            # Since we're querying it, might as well check how old it is and notify us
+            last_update = datetime.fromisoformat(values["updated"])
+            thirty_days = timedelta(days=30)
+            if datetime.now() > last_update + thirty_days:
+                if not thirty_day_warning_sent:
+                    # send webhook message informing it has been 30 days since last auth key update
+                    query = session.query(Settings).filter(Settings.key == "to_do_webhook").one_or_none()
+                    webhook = query.value if query else None
+
+                    async with aiohttp.ClientSession() as http_session:
+                        await http_session.post(
+                            webhook,
+                            json={
+                                "content": f"Twitch IRC Token last updated {values['updated']}. To update visit https://{environ['WEB_HOSTNAME']}/twitch/oauth",  # noqa E501
+                                "username": "TwitchBot",
+                            },
+                        )
+
+                thirty_day_warning_sent = True
+
+            else:
+                thirty_day_warning_sent = False
+
+        else:
+            print("Unable to find twitch irc token in database")
+
         print("Mysql keep-alive running...")
 
         await sleep(3600)  # Run the keep alive once an hour
