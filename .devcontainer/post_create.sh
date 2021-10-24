74c07e1cce33006a6b501386e33fb8ebbc9b2b1a
tisboyo <10230718+tisboyo@users.noreply.github.com>
2021-10-24 23:53:42 +0000

    More changes needed for #238 Fix dev environment issues after user change

--- .devcontainer/post_create.sh
+++ .devcontainer/post_create.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 pip install -r /workspace/requirements-dev.txt -r /workspace/bot/requirements.txt -r /workspace/web/requirements.txt
 pre-commit
+sudo ln /home/twitch_bot/.local/bin/black /usr/local/bin/black
